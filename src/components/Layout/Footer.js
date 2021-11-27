fab8dbefa4124cbfe5873d19b7a34c0f129980e2
BuildTools <unconfigured@null.spigotmc.org>
2021-11-26 21:35:57 -0500

    Add responsive footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,55 @@
 import React from "react";
 
+import * as styles from "../../styles/Layout/Footer.module.scss";
+
 const Footer = () => {
   return (
-    <>
-      <h2>Footer component</h2>
-    </>
+    <footer id={styles.footer}>
+      <div className={styles.content}>
+        <div className={styles.about}>
+          <h1>About Us</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend
+            suscipit mauris ac molestie. Fusce nec sollicitudin libero. Nullam
+            nisl sapien, blandit id tortor vitae, tempus facilisis nisi.
+          </p>
+          <br />
+          <p>
+            Fusce nec sollicitudin libero. Nullam nisl sapien, blandit id tortor
+            vitae, tempus facilisis nisi. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. In eleifend suscipit mauris ac
+            molestie.
+          </p>
+        </div>
+        <div className={styles.contact}>
+          <h1>Contact</h1>
+          <form name="contact" action="/success" method="POST">
+            <div id={styles.nameContainer}>
+              <label>
+                <span>Name</span>
+                <input type="text" name="name" required minLength="2" />
+              </label>
+            </div>
+            <div id={styles.emailContainer}>
+              <label>
+                <span>Email</span>
+                <input type="email" name="email" required />
+              </label>
+            </div>
+            <div id={styles.messageContainer}>
+              <label>
+                <span>Message</span>
+                <textarea name="message" cols="30" rows="8" required></textarea>
+              </label>
+            </div>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <p>&copy; Phoenix Tech 2021. All Rights Reserverd.</p>
+      </div>
+    </footer>
   );
 };
 
